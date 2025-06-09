0a756773ffa04225bbb44b70d673efad3d8f4a30
Shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2025-06-10 01:58:19 +0530

    Feature: select characters on click

--- src/Features/Games/Games.tsx
+++ src/Features/Games/Games.tsx
@@ -1,17 +1,22 @@
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import { games } from "../../Utils/Models/data";
+import { games, waldoPolygon } from "../../Utils/Models/data";
 import pointInPolygon from "point-in-polygon";
+import type { SelectPosType } from "./Utils/types";
+import Dropdown from "./Dropdown/Dropdown";
 
 const Games = () => {
   const [gameObj, setGameObj] = useState<
-    undefined | Record<string, string | number>
+    undefined | Record<string, string | number | unknown[]>
   >(undefined);
   const params = useParams();
+  const [selectPos, setSelectPos] = useState<null | SelectPosType>(null);
+
   useEffect(() => {
     const newGame = games.find((game) => game.gameId === Number(params.gameId));
     setGameObj(newGame);
   }, [params]);
+
   const handleClick: React.MouseEventHandler<HTMLImageElement> = (e) => {
     const viewportX = e.clientX;
     const viewportY = e.clientY;
@@ -26,17 +31,12 @@ const Games = () => {
     const imgY = viewportY - top;
     const imgOrginalX = Math.round(imgX * scaleX);
     const imgOrginalY = Math.round(imgY * scaleY);
-    const polygon = [
-      [939, 499],
-      [974, 550],
-      [1015, 589],
-      [982, 613],
-      [956, 641],
-      [922, 619],
-      [891, 603],
-      [907, 553],
-    ];
-    const isPointInPolygon = pointInPolygon([imgX, imgY], polygon);
+    const isPointInPolygon = pointInPolygon(
+      [imgOrginalX, imgOrginalY],
+      waldoPolygon,
+    );
+
+    setSelectPos({ x: imgX, y: imgY });
 
     console.log(`clicked position in viewport: `, {
       viewport: `(${viewportX},${viewportY})`,
@@ -45,20 +45,23 @@ const Games = () => {
       scale: `(${scaleX},${scaleY})`,
       imgOriginal: `(${imgOrginalX},${imgOrginalY})`,
       isPointInPolygon,
+      select: selectPos,
     });
   };
   return (
-    <div>
+    <div className="relative">
       {gameObj === undefined ? (
         <p>Loading...</p>
       ) : (
         <img
+          className="bg-zinc-900"
           onClick={handleClick}
           src={gameObj.imgURL as string}
           alt={gameObj.imgName as string}
-          key={gameObj.gameId}
+          key={gameObj.gameId as number}
         />
       )}
+      {selectPos && <Dropdown selectPos={selectPos} />}
     </div>
   );
 };
